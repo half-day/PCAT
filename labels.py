6f73ceeaf683c35f474233353cf867bd5caa8ada
icy blue <i@icys.top>
2024-03-17 16:13:56 +0800

    调整颜色随机代码

--- labels.py
+++ labels.py
@@ -1,27 +1,50 @@
 import json
-import math
 import random
-import time
 import colorsys
 import numpy as np
+from typing import Optional
+
+
+def random_rgb_colors(num_colors: int, seed: int = 0) -> 'np.array[num_colors, 3]':
+
+    def hsv_distance(hsv1: 'np.array[3]', hsv2: 'np.array[3]') -> float:
+        hsv1 = np.array(hsv1)
+        hsv2 = np.array(hsv2)
+        
+        xy1 = hsv1[1] * np.exp(1j * 2 * np.pi * hsv1[0])
+        xy2 = hsv2[1] * np.exp(1j * 2 * np.pi * hsv2[0])
+        
+        xyz1 = np.array([xy1.real, xy1.imag, hsv1[2]])
+        xyz2 = np.array([xy2.real, xy2.imag, hsv2[2]])
+        
+        return np.linalg.norm(xyz1 - xyz2)
+    
+    def random_hsv(seed: 'Optional[int]' = None) -> 'np.array[3]':
+        if seed is not None:
+            random.seed(seed)
+        h = random.random()
+        s = 0.2 + random.random() * 0.8
+        v = 0.2 + random.random() * 0.8
+        return np.array([h, s, v])
+    
+    random.seed(seed)
+    colors_hsv = []
+    pre = np.array([0, 0, 0])
+
+    for _ in range(num_colors):
+        now = random_hsv()
+        while hsv_distance(now, pre) < 1:
+            now = random_hsv()
+        pre = now
+        colors_hsv.append(now)
+
+    colors_rgb = [colorsys.hsv_to_rgb(hsv[0], hsv[1], hsv[2]) for hsv in colors_hsv]
+
+    return np.array(colors_rgb)
+
 
 with open('labels.json', 'r', encoding='utf-8') as f:
     labels_dict = json.load(f)
-
-
-# print(labels_dict)
-
-def hsv_distance(hsv1, hsv2):
-    x1 = math.cos(hsv1[0] * 2 * math.pi) * hsv1[1]
-    y1 = math.sin(hsv1[0] * 2 * math.pi) * hsv1[1]
-    z1 = hsv1[2]
-    x2 = math.cos(hsv2[0] * 2 * math.pi) * hsv2[1]
-    y2 = math.sin(hsv2[0] * 2 * math.pi) * hsv2[1]
-    z2 = hsv2[2]
-    dx = x1 - x2
-    dy = y1 - y2
-    dz = z1 - z2
-    return math.sqrt(dx * dx + dy * dy + dz * dz)
 
 
 class_labels = [item['label'] for item in labels_dict]
@@ -53,36 +76,4 @@ ui_groups = {
     'unlabel': [0],
 }
 
-random.seed(time.time())
-colors_hsv = [np.array([0, 0, 0])]
-pre = colors_hsv[-1]
-
-for i in range(100):
-    h = random.randint(0, 100) / 100
-    s = random.randint(20, 100) / 100
-    v = random.randint(20, 100) / 100
-    tmp = np.array([h, s, v])
-    while hsv_distance(tmp, pre) < 1:
-        h = random.randint(0, 100) / 100
-        s = random.randint(20, 100) / 100
-        v = random.randint(20, 100) / 100
-        tmp = np.array([h, s, v])
-    pre = tmp
-    colors_hsv.append(tmp)
-
-colors_rgb = [colorsys.hsv_to_rgb(hsv[0], hsv[1], hsv[2]) for hsv in colors_hsv]
-
-# colors_hex = [
-# '#000000',
-# '#B0C4DE', '#FF00FF', '#1E90FF', '#FA8072',
-# '#EEE8AA', '#FF1493', '#7B68EE', '#FFC0CB',
-# '#696969', '#556B2F', '#CD853F', '#000080',
-# '#32CD32', '#7F007F', '#B03060', '#800000',
-# '#483D8B', '#008000', '#3CB371', '#008B8B',
-# '#FF0000', '#FF8C00', '#FFD700', '#00FF00',
-# '#9400D3', '#00FA9A', '#DC143C', '#00FFFF',
-# '#00BFFF', '#0000FF', '#ADFF2F', '#DA70D6'
-# ]
-#
-# colors_rgb = [tuple(int(hex_color[i:i+2], 16) / 255.0 for i in (1, 3, 5)) for hex_color in colors_hex]
-# print(colors_rgb)
+colors_rgb = random_rgb_colors(100, seed=0)
